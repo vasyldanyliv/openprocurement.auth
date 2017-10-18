7244e18731eec0e7a37824360fc6246d56b18ad9
yarsanich <nezn27@gmail.com>
2017-10-18 16:24:29 +0300

    Prepare openprocurement.auth 2.0.1a1.dev4+eauctions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0.1a1.dev3+eauctions'
+version = '2.0.1a1.dev4+eauctions'
 
 setup(name='openprocurement.auth',
       version=version,
